eeee72936f4fb34ed4d91dd0e4bd3ee9dc0fd865
Gracia317 <graci4rm@gmail.com>
2026-05-06 22:38:15 +0300

    TD1N19.c

--- TD1N19.c
+++ TD1N19.c
@@ -2,32 +2,41 @@
 #include<stdio.h>
 int main ()
 {
-  int a, b, i;
+  int a, b, i, compte_diviseur=0;
   printf("Entrez un nombre");
   scanf("%d",&a);
   printf("Entrez un second nombre");
   scanf("%d",&b);
-  for(i=2;i<a;i++)
+  for(i=1;i<=a;i++)
   {
     if (a%i==0)
     {
-      printf("%d n'est pas premier.\n",a);
-    }
-    else
-    {
-      printf("%d est premier.\n",a);
+      compte_diviseur++;
     }
   }
-  for(i=2;i<b;i++)
+  if (compte_diviseur==2)
+  {
+    printf("%d est premier.\n",a);
+  }
+  else
+  {
+    printf("%d n'est pas premier.\n",a);
+  }
+  compte_diviseur=0;
+  for(i=1;i<=b;i++)
   {
     if (b%i==0)
     {
-      printf("%d n'est pas premier.\n",b);
-    }
-    else
-    {
-      printf("%d est premier.\n",b);
+      compte_diviseur++;
     }
   }
-  return 0;
+  if (compte_diviseur==2)
+  {
+    printf("%d est premier.\n",b);
+  }
+  else
+  {
+    printf("%d n'est pas premier.\n",a);
+  }
+  return (0);
 }
