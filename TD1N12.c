5fe8419b5775e6ef9c881149439f94cb0ffc96b0
Gracia317 <graci4rm@gmail.com>
2026-05-20 21:42:35 +0300

    TD1N12.c

--- TD1N12.c
+++ TD1N12.c
@@ -13,3 +13,24 @@ int main ()
   printf("1+2+3+..+%d=%d", N,somme);
   return 0;
 }
+int somme(int N);
+void affiche (int s);
+
+int somme(int N)
+{
+  int i;int somme;
+  printf("Entrez un entier N: ");
+  scanf("%d",&N);
+  somme=0;
+  for (i=1; i<N;i++)
+  {
+    somme=somme+i;
+  }
+  return(somme);
+}
+
+void affiche(int s)
+{
+  printf("La somme est: %d\n",s);
+}
+
