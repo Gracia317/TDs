619988c85d06b3450de993768bb72e0076a692a5
Gracia317 <graci4rm@gmail.com>
2026-05-29 20:19:44 +0300

    TD2N7.c

--- TD2N7.c
+++ TD2N7.c
@@ -2,22 +2,50 @@
 (de même dimension).*/
 #include<stdio.h>
 
-int scalaire(int *u[2] ,*v[2])
+void recup (int *n);
+void u_et_v(int *u, int *v, int n);
+int scalaire2(int u[], int v[], int n);
+
+void recup(int *n)
 {
-  int scalaire;
-  scalaire=(*u[0])*(*v[0])+(*u[1])*(*v[1]);
-  return (scalaire);
+  printf("Dimension de votre tableau?: ");
+  scanf("%d", n);
 }
-int main ()
+
+void u_et_v(int *u, int *v, int n)
 {
-  int scalaire;
-  printf("Entrez les composants du vecteur u:\n");
-  scanf("%d",(u+0));
-  scanf("%d",(u+1));
-  printf("Entrez les composants du vecteur v:\n");
-  scanf("%d",(v+0));
-  scanf("%d",(v+1));
-  scalaire=scalaire(*(u+0),*(u+1),*(v+0),*(v+1));
-  printf("produit scalaire de u et v: %d\n", scalaire);
+  int i;
+  printf("les composantes de u:\n");
+  for (i=0; i<n; i++)
+  {
+    scanf("%d", (u+i) );
+  }
+  printf("les composantes de v:\n");
+  for (i=0; i<n; i++)
+  {
+    scanf("%d", (v+i) );
+  }
+}
+
+int scalaire2(int u[], int v[], int n)
+{
+  int r=0;
+  while ( n != 0 )
+    {
+      r=r+u[n-1]*v[n-1];
+      n--;
+    }
+  return r;
+}
+
+int main()
+{
+  int n;
+  int valiny;
+  recup(&n);
+  int u[n], v[n];
+  u_et_v(u, v, n);
+  valiny=scalaire2(u, v, n);
+  printf("produit scalaire u et v= %d\n", valiny);
   return 0;
 }
